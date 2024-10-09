49a98408dfb9ab25beb33fd443cda1e47d954b77
jcwilli <cadfanw@gmail.com>
2024-10-09 12:51:46 +0100

    added phone number validation

--- src/main/java/com/example/moduleauthsecurityjava/controllers/PhoneNumberController.java
+++ src/main/java/com/example/moduleauthsecurityjava/controllers/PhoneNumberController.java
@@ -1,5 +1,7 @@
 package com.example.moduleauthsecurityjava.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.example.moduleauthsecurityjava.models.PhoneNumberRequest;
@@ -8,8 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PhoneNumberController {
     @PostMapping("/phone-number")
-    public String phoneNumberSubmit(@ModelAttribute PhoneNumberRequest request) {
-        System.out.println("\033[96mThe user submitted the phone number: " + request.getPhone() + "\033[0m");
-        return "Successful!";
+    public ResponseEntity<String> phoneNumberSubmit(@ModelAttribute PhoneNumberRequest request) {
+        if(isValidPhoneNumber(request)) {
+            System.out.println("\033[96mThe user submitted the phone number: " + request.getPhone() + "\033[0m");
+            return ResponseEntity.ok("Successful");
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid Phone Number");
+        }
+
+    }
+
+    private boolean isValidPhoneNumber(PhoneNumberRequest request){
+        return !request.getPhone().isEmpty() && request.getPhone().length() >= 10 && request.getPhone().length() <= 11;
     }
 }
